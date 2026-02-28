a5381b661a213fdda7f5ee4a2661cd7b1a2f0185
joehman <jonte.oehman@gmail.com>
2026-02-28 12:43:11 +0100

    working on website

--- index.js
+++ index.js
@@ -2,9 +2,17 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-    res.send('Hello, World!')
-})
+const publicpath = require('path').resolve('public')
+
+function get(reqpath, filepath) {
+    app.get(reqpath, (req, res) => {
+        res.sendFile(filepath, { root: publicpath })
+    })
+}
+
+app.use(express.static(publicpath))
+
+get("/", 'home.html');
 
 app.listen(port, () => {
     console.log('Server running on port ' + port);
